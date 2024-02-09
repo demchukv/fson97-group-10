d8ec37eef1f1000a2bed83243cc0371152eb9771
demchukv <demchukv@gmail.com>
2024-02-09 15:34:09 +0000

    Deploying to gh-pages from @ demchukv/fson97-group-10@40d6fe096585fa5fd372e1443865770ef5ca3039 🚀

--- assets/main-a4babb2f.js
+++ assets/main-a4babb2f.js
@@ -44,7 +44,7 @@ import{i as E,a as b,P as U}from"./vendor-e8675f53.js";(function(){const t=docum
       <div class="rating-container">
         <p class="modal-exercises-rating">${R}</p>
         <svg class="star" width="18" height="18">
-          <use href="${f}.svg#icon-star"></use>
+          <use href="${f}svg#icon-star"></use>
         </svg>
       </div>
       <ul class="description-list">
@@ -79,4 +79,4 @@ import{i as E,a as b,P as U}from"./vendor-e8675f53.js";(function(){const t=docum
       <button data-id="${e}" class="give-rating-btn">Give a rating</button>
     </div>
   </div>`}
-//# sourceMappingURL=main-152d799b.js.map
+//# sourceMappingURL=main-a4babb2f.js.map
