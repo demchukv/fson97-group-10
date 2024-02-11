b64269485acf6ad54a96526adbf04f167dbcb6b9
Sashiy1 <aleksander.paluch@wp.pl>
2024-02-11 19:08:38 +0100

    js update

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,22 +1,27 @@
 const quoteContainer = document.querySelector('.generated-quote-container');
+const start = 10;
+const date = new Date();
+const today = date.getDate();
+console.log(today);
 
+if (start !== today) {
+  document.addEventListener('DOMContentLoaded', aaddQuote);
 
-document.addEventListener('DOMContentLoaded', aaddQuote);
-
-function aaddQuote() {
-  fetch('https://energyflow.b.goit.study/api/quote', {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-    },
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data); 
-      localStorage.setItem('quoteData', JSON.stringify(data))
-    
+  function aaddQuote() {
+    fetch('https://energyflow.b.goit.study/api/quote', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
     })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        localStorage.setItem('quoteData', JSON.stringify(data));
+        
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 }
