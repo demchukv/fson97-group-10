84bed157d181d4bfb284eb8b979875d6b57c5e1f
demchukv <demchukv@gmail.com>
2024-02-11 18:08:31 +0000

    Deploying to gh-pages from @ demchukv/fson97-group-10@4a5d6bdb64cd393f9b0f3474a5ad2be1c0ea076c 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,37 +1,53 @@
-import{i as s}from"./assets/scroll-up-681e4ae3.js";import"./assets/vendor-ecc6328a.js";const p={favoritesContent:document.querySelector(".favorites-card-content")},t="favorites";let r=null;try{let e=localStorage.getItem(t),a=JSON.parse(e);p.favoritesContent.innerHTML=`<ul class="favorites-group">${d(a)}</ul>`,r=document.querySelectorAll(".favorites-delete")}catch(e){console.log(e)}r&&r.forEach(e=>{e.addEventListener("click",a=>{let i=localStorage.getItem(t),n=JSON.parse(i);const o=a.target.closest(".favorites-item"),c=o.dataset.id;localStorage.setItem(t,JSON.stringify(n.filter(({_id:l})=>l!==c))),document.querySelector(".favorites-group").removeChild(o)})});function d(e){return e.map(a=>`
-        <li class="favorites-item" data-id="${a._id}">
+import{i}from"./assets/scroll-up-947c3626.js";import"./assets/vendor-ecc6328a.js";const n={favoritesContent:document.querySelector(".favorites-card-content")},a="favorites";let t=null;try{if(localStorage.getItem(a)){const s=JSON.parse(localStorage.getItem(a));Array.isArray(s)&&s.length===0?console.log(""):n.favoritesContent.innerHTML=`<ul class="favorites-group">${d(s)}</ul> 
+
+    `}t=document.querySelectorAll(".favorites-delete")}catch(s){console.log(s)}t&&t.forEach(s=>{s.addEventListener("click",e=>{let c=localStorage.getItem(a),l=JSON.parse(c);const r=e.target.closest(".favorites-item"),p=r.dataset.id;localStorage.setItem(a,JSON.stringify(l.filter(({_id:f})=>f!==p)));const o=document.querySelector(".favorites-group");o.removeChild(r),o.children.length===0&&(n.favoritesContent.innerHTML=` <div class="favorites-content">
+        <img
+          srcset="./public/favorites.png 1x, ./public/favorites.2x.png 2x"
+          src="./public/favorites.png"
+          alt="Image favorites"
+          width="85"
+          height="52"
+        />
+
+        <p class="favorites-text">
+          It appears that you haven't added any exercises to your favorites yet.
+          To get started, you can add exercises that you like to your favorites
+          for easier access in the future.
+        </p>
+      </div>`)})});function d(s){return s.map(e=>`
+        <li class="favorites-item" data-id="${e._id}">
            <p class="favorites-item-head">
               <span class="favorites-item-head-wrapper">
                 <span class="favorites-item-workout">WORKOUT</span>
                   <button class="favorites-delete">
                     <svg class="delete-icon" width="12" height="13">
-                      <use href="${s}#icon-trash"></use>
+                      <use href="./img/icons.svg#icon-trash"></use>
                     </svg>
                   </button>
-                  <a class="ex-item-start" href="#" data-id="${a._id}">
+                  <a class="ex-item-start" href="#" data-id="${e._id}">
                     <span>Start</span>
                      <svg class="favorites-arrow-icon" width="14" height="14">
-                      <use href="${s}#icon-arrow-start"></use>
+                      <use href="${i}#icon-arrow-start"></use>
                     </svg>
                   </a>
              </p>
               <span class="favorites-card-title">
                 <span class="favorites-run-men"><svg class="ex-icon-run" width="14" height="14">
-                  <use href="${s}#icon-running_man"></use>
+                  <use href="${i}#icon-running_man"></use>
                 </svg>
               </span>
-            <h3 class="favorites-item-name">${a.name.charAt(0).toUpperCase()+a.name.slice(1)}</h3>
+            <h3 class="favorites-item-name">${e.name.charAt(0).toUpperCase()+e.name.slice(1)}</h3>
             </span>
             </span>
             <p class="favorites-item-info">
              <span class="ex-info-group"><span class="favorites-item-desc">Burned calories:</span> <span
-                class="favorites-item-value">${a.burnedCalories} / ${a.time} min</span>
+                class="favorites-item-value">${e.burnedCalories} / ${e.time} min</span>
         </span>
         <span class="favorites-info-group"><span class="favorites-item-desc">Body part:</span> <span
-                class="favorites-item-value">${a.bodyPart.charAt(0).toUpperCase()+a.bodyPart.slice(1)}</span>
+                class="favorites-item-value">${e.bodyPart.charAt(0).toUpperCase()+e.bodyPart.slice(1)}</span>
         </span>
         <span class="favorites-info-group"><span class="favorites-item-desc">Target:</span> <span
-                class="favorites-item-value">${a.target.charAt(0).toUpperCase()+a.target.slice(1)}</span>
+                class="favorites-item-value">${e.target.charAt(0).toUpperCase()+e.target.slice(1)}</span>
         </span>
     </p>
 </li>
