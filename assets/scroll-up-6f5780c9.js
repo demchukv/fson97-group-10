729a6fde5b1b62d47ec942a8998ccd0962c5fdcb
demchukv <demchukv@gmail.com>
2024-02-11 09:05:26 +0000

    Deploying to gh-pages from @ demchukv/fson97-group-10@6e952321096f31f12fa0fd5f1fcc588809d59bf1 🚀

--- assets/scroll-up-6f5780c9.js
+++ assets/scroll-up-6f5780c9.js
@@ -59,4 +59,4 @@ import{i as S,a as T}from"./vendor-ecc6328a.js";(function(){const t=document.cre
         </div>
       </div>
     </div>`}const $=document.querySelector(".scroll-up-btn");window.addEventListener("scroll",ie);function ie(){window.scrollY>200?$.classList.add("scroll-up-btn--show"):$.classList.remove("scroll-up-btn--show")}$.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})});export{f as g,y as i,ce as p,v as s};
-//# sourceMappingURL=scroll-up-e219da6b.js.map
+//# sourceMappingURL=scroll-up-6f5780c9.js.map
