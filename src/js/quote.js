3c7fec5f9f70146d5a4dee04e6d311b156eaa84b
Sashiy1 <aleksander.paluch@wp.pl>
2024-02-11 20:31:20 +0100

    quote render added

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,27 +1,49 @@
 const quoteContainer = document.querySelector('.generated-quote-container');
-const start = 10;
-const date = new Date();
-const today = date.getDate();
-console.log(today);
 
-if (start !== today) {
-  document.addEventListener('DOMContentLoaded', aaddQuote);
+document.addEventListener('DOMContentLoaded', addOrUpdateQuote);
 
-  function aaddQuote() {
-    fetch('https://energyflow.b.goit.study/api/quote', {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-      },
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        localStorage.setItem('quoteData', JSON.stringify(data));
-        
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+function addOrUpdateQuote() {
+
+  const storedData = localStorage.getItem('quoteData');
+  const storedDate = localStorage.getItem('quoteDate');
+
+  const today = new Date().getDate();
+
+
+  if (storedData && storedDate && parseInt(storedDate) === today) {
+    const { author, quote } = JSON.parse(storedData);
+    createMarkup(author, quote);
+  } else {
+
+    fetchNewQuote();
   }
+}
+
+function fetchNewQuote() {
+  fetch('https://energyflow.b.goit.study/api/quote', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+
+      localStorage.setItem('quoteData', JSON.stringify(data));
+      localStorage.setItem('quoteDate', new Date().getDate());
+
+      const { author, quote } = data;
+      createMarkup(author, quote);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+function createMarkup(author, quote) {
+  const markup = `
+    <h2 class="quote-header">Quote of the day</h2>
+    <p class="quote-text">${quote}</p>
+    <h3 class="quote-author">${author}</h3>`;
+  quoteContainer.innerHTML = markup;
 }
