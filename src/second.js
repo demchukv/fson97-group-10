90cdc463838f3f5c5de9ad8280a705ca12f17b03
demchukv <demchukv@gmail.com>
2024-02-11 09:17:26 +0200

    change in favorites load files

--- src/second.js
+++ src/second.js
@@ -1,4 +1,6 @@
 import { showAlert, getLoader } from './js/common.js';
 import './js/header.js';
 import './js/favorites.js';
+import './js/modalExercises.js';
+import './js/give-rating.js';
 import './js/scroll-up.js';
