a902d42d23f32fc1b25026909cfdf822c52d93e5
demchukv <demchukv@gmail.com>
2024-02-12 16:01:18 +0000

    Deploying to gh-pages from @ demchukv/fson97-group-10@1d7b73bd6ad2e98c1d6b62e6f6fa706c118ae8cf 🚀

--- assets/scroll-up-c52a9197.js
+++ assets/scroll-up-c52a9197.js
@@ -78,4 +78,4 @@ import{i as R,a as D}from"./vendor-e8675f53.js";(function(){const t=document.cre
         </div>
       </div>
     </div>`}const M=document.querySelector(".scroll-up-btn");window.addEventListener("scroll",ue);function ue(){window.scrollY>200?M.classList.add("scroll-up-btn--show"):M.classList.remove("scroll-up-btn--show")}M.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})});export{p as g,S as i,fe as p,a as s};
-//# sourceMappingURL=scroll-up-1e66b3e6.js.map
+//# sourceMappingURL=scroll-up-c52a9197.js.map
--- commonHelpers.js
+++ commonHelpers.js
@@ -1,39 +1,39 @@
-import{i as t}from"./assets/scroll-up-1e66b3e6.js";import"./assets/vendor-e8675f53.js";const n={favoritesContent:document.querySelector(".favorites-card-content")},a="favorites";let r=null;try{if(localStorage.getItem(a)){const s=JSON.parse(localStorage.getItem(a));Array.isArray(s)&&s.length===0?console.log(""):n.favoritesContent.innerHTML=`<ul class="favorites-group">${d(s)}</ul> 
+import{i as r}from"./assets/scroll-up-c52a9197.js";import"./assets/vendor-e8675f53.js";const t={favoritesContent:document.querySelector(".favorites-card-content")},a="favorites";let o=null;const i=` <div class="favorites-content">
+<img
+  srcset="./public/favorites.png 1x, ./public/favorites.2x.png 2x"
+  src="./public/favorites.png"
+  alt="Image favorites"
+  width="85"
+  height="52"
+/>
 
-    `}r=document.querySelectorAll(".favorites-delete")}catch(s){console.log(s)}r&&r.forEach(s=>{s.addEventListener("click",e=>{let c=localStorage.getItem(a),l=JSON.parse(c);const o=e.target.closest(".favorites-item"),p=o.dataset.id;localStorage.setItem(a,JSON.stringify(l.filter(({_id:f})=>f!==p)));const i=document.querySelector(".favorites-group");i.removeChild(o),i.children.length===0&&(n.favoritesContent.innerHTML=` <div class="favorites-content">
-        <img
-          srcset="./public/favorites.png 1x, ./public/favorites.2x.png 2x"
-          src="./public/favorites.png"
-          alt="Image favorites"
-          width="85"
-          height="52"
-        />
+<p class="favorites-text">
+  It appears that you haven't added any exercises to your favorites yet.
+  To get started, you can add exercises that you like to your favorites
+  for easier access in the future.
+</p>
+</div>`;try{if(localStorage.getItem(a)){const s=JSON.parse(localStorage.getItem(a));Array.isArray(s)&&s.length===0?(console.log(""),t.favoritesContent.innerHTML=i):t.favoritesContent.innerHTML=`<ul class="favorites-group">${v(s)}</ul> 
 
-        <p class="favorites-text">
-          It appears that you haven't added any exercises to your favorites yet.
-          To get started, you can add exercises that you like to your favorites
-          for easier access in the future.
-        </p>
-      </div>`)})});function d(s){return s.map(e=>`
+    `,o=document.querySelectorAll(".favorites-delete")}else t.favoritesContent.innerHTML=i}catch(s){console.log(s)}o&&o.forEach(s=>{s.addEventListener("click",e=>{let l=localStorage.getItem(a),p=JSON.parse(l);const n=e.target.closest(".favorites-item"),f=n.dataset.id;localStorage.setItem(a,JSON.stringify(p.filter(({_id:d})=>d!==f)));const c=document.querySelector(".favorites-group");c.removeChild(n),c.children.length===0&&(t.favoritesContent.innerHTML=i)})});function v(s){return s.map(e=>`
         <li class="favorites-item" data-id="${e._id}" id="card-${e._id}">
            <p class="favorites-item-head">
               <span class="favorites-item-head-wrapper">
                 <span class="favorites-item-workout">WORKOUT</span>
                   <button class="favorites-delete">
                     <svg class="delete-icon" width="12" height="13">
-                      <use href="${t}#icon-trash"></use>
+                      <use href="${r}#icon-trash"></use>
                     </svg>
                   </button>
                   <a class="ex-item-start" href="" data-id="${e._id}">
                     <span>Start</span>
                      <svg class="favorites-arrow-icon" width="14" height="14">
-                      <use href="${t}#icon-arrow-start"></use>
+                      <use href="${r}#icon-arrow-start"></use>
                     </svg>
                   </a>
              </p>
               <span class="favorites-card-title">
                 <span class="favorites-run-men"><svg class="ex-icon-run" width="14" height="14">
-                  <use href="${t}#icon-running_man"></use>
+                  <use href="${r}#icon-running_man"></use>
                 </svg>
               </span>
             <h3 class="favorites-item-name">${e.name.charAt(0).toUpperCase()+e.name.slice(1)}</h3>
