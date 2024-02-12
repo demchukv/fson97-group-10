318de33d68fdcd204a7968228b316fc352a8f394
demchukv <demchukv@gmail.com>
2024-02-12 08:47:24 +0200

    remove listener from quote.js

--- src/second.js
+++ src/second.js
@@ -1,6 +1,7 @@
 import { showAlert, getLoader } from './js/common.js';
 import './js/header.js';
 import './js/hero.js';
+import './js/quote.js';
 import './js/favorites.js';
 import './js/modalExercises.js';
 import './js/give-rating.js';
