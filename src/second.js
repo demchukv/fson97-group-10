81887a570914d149fbab7ee8d80c42647b0fb24c
demchukv <demchukv@gmail.com>
2024-02-12 08:51:51 +0200

    modals

--- src/second.js
+++ src/second.js
@@ -1,6 +1,5 @@
 import { showAlert, getLoader } from './js/common.js';
 import './js/header.js';
-import './js/hero.js';
 import './js/quote.js';
 import './js/favorites.js';
 import './js/modalExercises.js';
